b881d40aed0c96cfe0cce0c582cdfe5d8826fa94
Jordan <jordan.b3360@gmail.com>
2022-09-18 17:37:34 -0500

    working on the login portion, currently still testing, works successfully in insomnia

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-// const { Posts, User } = require("../models");
-// const withAuth = require("../utils/auth");
+const { Posts, User } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Home route
 router.get("/", async (req, res) => {
@@ -10,7 +10,33 @@ router.get("/", async (req, res) => {
 // posts with param route
 router.get("/posts/:id", async (req, res) => {});
 
+// Use withAuth middleware to prevent access to route
+router.get("/test", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      // include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("homepage", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // login route on home page
-router.get("/login", (req, res) => {});
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
 
 module.exports = router;
--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,11 @@
 // auth helper will go here
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
