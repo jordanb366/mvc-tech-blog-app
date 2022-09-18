84004a4ebfa4fda4d236904bb65beb4b59baf954
Jordan <jordan.b3360@gmail.com>
2022-09-17 22:17:25 -0500

    worked on boilerplate code for model index.js and for User.js, has hooks to use bcrypt for password hashing  as well in user  model for best practice vs just having them in the routes

--- models/index.js
+++ models/index.js
@@ -1,1 +1,11 @@
 // index models will go here
+const User = require("./User");
+// posts for blog may go here once logins are working
+
+User.hasMany({
+  //  Need some type of foreign key for blog posts
+});
+
+// blog post will belong to users most likely
+
+module.exports = { User };
