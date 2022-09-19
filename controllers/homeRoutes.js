f610a07589fc8de1c65e0184a9db903835b4a256
Jordan <jordan.b3360@gmail.com>
2022-09-19 16:53:49 -0500

    posts now show up homepage, and posts have link to actual post now

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,13 +4,51 @@ const withAuth = require("../utils/auth");
 
 // Home route
 router.get("/", async (req, res) => {
-  res.render("homepage", {
-    logged_in: req.session.logged_in,
-  });
+  try {
+    // Get all posts and JOIN with user data
+    const postsData = await Posts.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postsData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // posts with param route
-router.get("/posts/:id", async (req, res) => {});
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const postsData = await Posts.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const posts = postsData.get({ plain: true });
+
+    res.render("post", {
+      ...posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Use withAuth middleware to prevent access to route
 router.get("/dashboard", withAuth, async (req, res) => {
@@ -33,7 +71,7 @@ router.get("/dashboard", withAuth, async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get("/post", withAuth, async (req, res) => {
+router.get("/newpost", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -43,7 +81,7 @@ router.get("/post", withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
     console.log(user);
-    res.render("post", {
+    res.render("newpost", {
       ...user,
       logged_in: true,
     });
