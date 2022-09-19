dffce6786c8425c1bf3e16c732eceef52d2ee33b
Jordan <jordan.b3360@gmail.com>
2022-09-18 20:02:50 -0500

    added dashboard page, figured out how to hide/show logout button if logged in, had to add frontend code for logout button to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,24 +4,26 @@ const withAuth = require("../utils/auth");
 
 // Home route
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  res.render("homepage", {
+    logged_in: req.session.logged_in,
+  });
 });
 
 // posts with param route
 router.get("/posts/:id", async (req, res) => {});
 
 // Use withAuth middleware to prevent access to route
-router.get("/test", withAuth, async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
-      // include: [{ model: Project }],
+      // include: [{ model: Posts }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render("homepage", {
+    res.render("dashboard", {
       ...user,
       logged_in: true,
     });
